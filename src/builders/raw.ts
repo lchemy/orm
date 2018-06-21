b42b3f9e24d4f82ddda61a682855c8437e74755d
Jesse Zhang <jesse.zhang@tapad.com>
2018-06-21 13:15:36 -0400

    feat: add wrapped raw cast functions

--- src/builders/raw.ts
+++ src/builders/raw.ts
@@ -7,6 +7,14 @@ export interface WrappedRawBuilder {
 	currentDate: WrappedRaw;
 	currentTime: WrappedRaw;
 	currentTimestamp: WrappedRaw;
+
+	asBoolean: (value: any) => WrappedRaw;
+	asString: (value: any) => WrappedRaw;
+	asInt: (value: any) => WrappedRaw;
+	asBigInt: (value: any) => WrappedRaw;
+	asFloat: (value: any) => WrappedRaw;
+	asDate: (value: any) => WrappedRaw;
+	asTimestamp: (value: any) => WrappedRaw;
 }
 
 export const raw = (() => {
@@ -18,6 +26,18 @@ export const raw = (() => {
 	fn.currentDate = fn("CURRENT_DATE");
 	fn.currentTime = fn("CURRENT_TIME");
 	fn.currentTimestamp = fn("CURRENT_TIMESTAMP");
+
+	const castFnBuilder = (type: string) => {
+		return (value: any) => fn(`CAST(? AS ${ type })`, [value]);
+	};
+
+	fn.asBoolean = castFnBuilder("BOOLEAN");
+	fn.asString = castFnBuilder("STRING");
+	fn.asInt = castFnBuilder("INT");
+	fn.asBigInt = castFnBuilder("BIGINT");
+	fn.asFloat = castFnBuilder("FLOAT");
+	fn.asDate = castFnBuilder("DATE");
+	fn.asTimestamp = castFnBuilder("TIMESTAMP");
 
 	return fn as WrappedRawBuilder;
 })();
