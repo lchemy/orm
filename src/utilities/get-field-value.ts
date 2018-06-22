5c1260df2838357cb7c13e5819849f65abb21b25
Jesse Zhang <jesse.zhang@tapad.com>
2018-06-22 12:08:32 -0400

    fix: allow update null to work correctly

--- src/utilities/get-field-value.ts
+++ src/utilities/get-field-value.ts
@@ -5,7 +5,7 @@ import { getFromPath } from "./get-from-path";
 export function getFieldValue(field: ColumnField, item: object): any {
 	const { path, alias } = field["🜁"],
 		value = getFromPath(item, path.slice(1));
-	if (value != null) {
+	if (value !== undefined) {
 		return value;
 	} else if (alias != null) {
 		return getFromPath(item, alias["🜁"].path.slice(1));
